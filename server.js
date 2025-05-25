d0bd2fa79ef2e33720c58643f5bdcd64d4d5c641
Sachin Gwala <114350906+GwSachin@users.noreply.github.com>
2025-05-26 03:09:31 +0530

    Change Sequence

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(cors())
 app.get('/',(req, res) => res.status(200).json({ status: 'OK',message:"Server Run Successfully" }))
+app.get('/health', (req, res) => res.status(200).json({ status: 'OK',message:"Status Successful" }));
 app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
-app.get('/health', (req, res) => res.status(200).json({ status: 'OK',message:"Status Successful" }));
 
 app.use(notFound);
 // error handler
